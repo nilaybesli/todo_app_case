ebe8cf698c523036fcf6ad1a5c0a79312009c518
nilaybesli <64665975+nilaybesli@users.noreply.github.com>
2024-07-28 18:22:07 +0300

    add todo

---
+++ lib/controllers/todo_controller.dart
@@ -0,0 +1,42 @@
+import 'package:get/get.dart';
+import 'package:todo_app_case/data/models/todo_model.dart';
+import '../data/services/todo_service.dart';
+
+class TodoController extends GetxController {
+  var todoList = <Todo>[].obs;
+  var isLoading = true.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchTodos();
+  }
+
+  void fetchTodos() async {
+    isLoading(true);
+    try {
+      var todos = await TodoService().getTodos();
+      todoList.assignAll(todos);
+      // rxlist metodu, mevcut içeriği temizler tüm öğeleri listeler
+    } catch (e) {
+      Get.snackbar('Error', e.toString());
+    } finally {
+      isLoading(false);
+    }
+  }
+
+  void addTodo(Todo todo) async {
+    await TodoService().addTodo(todo);
+    fetchTodos();
+  }
+
+  void updateTodo(Todo todo) async {
+    await TodoService().updateTodo(todo);
+    fetchTodos();
+  }
+
+  void deleteTodo(String id) async {
+    await TodoService().deleteTodo(id);
+    fetchTodos();
+  }
+}
---
+++ lib/ui/views/add_todo_screen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo_app_case/controllers/todo_controller.dart';
+import 'package:todo_app_case/data/models/todo_model.dart';
+
+class AddTodoScreen extends StatefulWidget {
+  final Todo? todo;
+
+  const AddTodoScreen({super.key, this.todo});
+
+  @override
+  State<AddTodoScreen> createState() => _AddTodoScreenState();
+}
+
+class _AddTodoScreenState extends State<AddTodoScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+
+  final TodoController todoController = Get.find();
+
+  void addTodo() {
+    if (_formKey.currentState!.validate()) {
+      todoController.addTodo(
+        Todo(
+            id: DateTime.now().toString(),
+            title: _titleController.text,
+            description: _descriptionController.text,
+            createdAt: DateTime.now()),
+      );
+    }
+    Get.back();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Todo'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _titleController,
+                decoration: const InputDecoration(labelText: 'Title'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a title';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _descriptionController,
+                decoration: const InputDecoration(labelText: 'Description'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a description';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(onPressed: addTodo, child: const Text("Add"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
+  }
+}
